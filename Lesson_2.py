a1e1bbe407d511e29df863d2be922bfcbbd7282d
malinin <malinin-vlad@yan.dex.ru>
2023-03-21 22:00:14 +0300

    ДЗ 2

---
+++ Lesson_2.py
@@ -0,0 +1,16 @@
+uchastnik_spora = [
+{"name": 'ООО "Рога и Копыта"', "status": "Истец", "inn": "4545454545"},
+{"name": 'Баширов А.А.', "status": "Ответчик", "inn": "32323232323232"},
+{"name": 'Петров А.А.', "status": "Третье лицо", "inn": "12121212121212"}
+]
+
+name = input("Введите имя участника спора: ")
+status = input("Введите статус участника спора (Истец/Ответчик/Третье лицо): ")
+inn = input("Введите ИНН участника спора: ")
+
+
+new_uchastnik = {"name": name, "status": status, "inn": inn}
+
+uchastnik_spora.append(new_uchastnik)
+
+print(uchastnik_spora)
