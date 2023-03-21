db698d4ae69a206cf6d01bb749bf74bc20da4143
malinin <malinin-vlad@yan.dex.ru>
2023-03-21 20:30:02 +0300

    ДЗ 1

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,1 +1,21 @@
-#test_2
+#ДЗ 1
+
+
+#2
+sec = input('Введите количество секунд: ')
+
+if not sec.isdigit():
+    print("Ошибка: введено не число.")
+else:
+    sec = int(sec)
+    min = sec/60
+    hour = min/60
+    print('Количество секунд: ', sec)
+    print('Количество минут: ', min)
+    print('Количество часов: ', hour)
+
+#3
+n = input('Введите число от 1 до 9: ')
+nn = n + n
+nnn = n + n + n
+print(int(n) + int(nn) + int(nnn))
